ff352347cca555daef1b776f4bddfdaab50a9a5e
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-12-05 22:37:19 +0000

    feat(links): add more images plus angry buns

--- LeafBot/LeafBot/Data/Links.cs
+++ LeafBot/LeafBot/Data/Links.cs
@@ -4,6 +4,7 @@
   {
     // sleepy bunnies
     public static string[ ] SLEEPY_BUNS = {
+      "https://user-images.githubusercontent.com/7717434/101266456-f0ac6380-3746-11eb-8b22-c97c45e93dcb.jpg",
       "https://user-images.githubusercontent.com/7717434/101260094-36583480-3725-11eb-8e66-71887dc2ab3d.jpg",
       "https://user-images.githubusercontent.com/7717434/101260102-45d77d80-3725-11eb-8c92-f04de201cbcc.jpg",
       "https://user-images.githubusercontent.com/7717434/101260124-73bcc200-3725-11eb-8736-4b283e7787c8.jpg",
@@ -11,9 +12,15 @@
       "https://user-images.githubusercontent.com/7717434/101260947-525ed480-372b-11eb-923f-2dfcd0bd26e3.jpg",
       "https://user-images.githubusercontent.com/7717434/101261015-da44de80-372b-11eb-9431-17029e5acbd8.jpg",
       "https://user-images.githubusercontent.com/7717434/101260995-aff32100-372b-11eb-9e12-88b740d58e53.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266404-68c65980-3746-11eb-8db6-fa904a5d320e.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266434-cbb7f080-3746-11eb-9fca-bb26063adfeb.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266443-dd00fd00-3746-11eb-8933-79473cfdb053.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266584-f22a5b80-3747-11eb-8d52-a0f5e6db8023.jpg",
+
     };
 
     public static string[ ] CURIOUS_BUNS = {
+      "https://user-images.githubusercontent.com/7717434/101266357-0bcaa380-3746-11eb-88fb-b403e0c5ad89.jpg",
       "https://user-images.githubusercontent.com/7717434/101261147-eda47980-372c-11eb-8955-7d7627f60823.jpg",
       "https://user-images.githubusercontent.com/7717434/101261156-06ad2a80-372d-11eb-9053-847da44e5181.jpg",
       "https://user-images.githubusercontent.com/7717434/101261163-0d3ba200-372d-11eb-8030-6ff0d92e36eb.jpg",
@@ -21,8 +28,20 @@
       "https://user-images.githubusercontent.com/7717434/101261230-9357e880-372d-11eb-9c82-a74a47754c04.jpg",
       "https://user-images.githubusercontent.com/7717434/101261273-e893fa00-372d-11eb-99f2-814bfa991d67.jpg",
       "https://user-images.githubusercontent.com/7717434/101261280-f9447000-372d-11eb-9b14-13388c921971.jpg",
-      "https://user-images.githubusercontent.com/7717434/101261309-314bb300-372e-11eb-9bed-cee8699a8f2f.jpg",
-      "https://user-images.githubusercontent.com/7717434/101261318-41fc2900-372e-11eb-8b09-fc516db4e7e1.jpg"
+      "https://user-images.githubusercontent.com/7717434/101261318-41fc2900-372e-11eb-8b09-fc516db4e7e1.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266357-0bcaa380-3746-11eb-88fb-b403e0c5ad89.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266393-4fbda880-3746-11eb-978b-10f7d532aca3.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266393-4fbda880-3746-11eb-978b-10f7d532aca3.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266559-a7a8df00-3747-11eb-88fe-79bcb426d783.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266634-97ddca80-3748-11eb-88be-9d2b3f876906.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266635-9a402480-3748-11eb-8296-a4ab68764b70.jpg",
+    };
+
+    public static string[ ] ANGRY_BUNS = {
+      "https://user-images.githubusercontent.com/7717434/101266465-face6200-3746-11eb-9eec-828ddf15f061.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266467-fd30bc00-3746-11eb-8b8a-737e8cef1109.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266563-b1324700-3747-11eb-8a9a-1ee61e32cae1.jpg",
+      "https://user-images.githubusercontent.com/7717434/101266627-84cafa80-3748-11eb-8f8f-0a18614fffdb.jpg",
     };
   }
 }
