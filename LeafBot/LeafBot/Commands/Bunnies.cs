e71bf48cc02dc9639dd60ea4439ade2c1ed4d6d7
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-12-05 18:44:11 +0000

    feat(bunnies): sleepy bun command

---
+++ LeafBot/LeafBot/Commands/Bunnies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using LeafBot.Data;
+
+namespace LeafBot.Commands
+{
+  public class Bunnies : BaseCommandModule
+  {
+    private Random rng = new Random();
+
+    [Command("sleepy")]
+    [Description("blesses you with a photo of a sleepy bun")]
+    public async Task Sleepy(CommandContext ctx)
+    {
+      var embed = new DiscordEmbedBuilder
+      {
+        ImageUrl = Links.SLEEPY_BUNS[rng.Next(Links.SLEEPY_BUNS.Length)]
+      };
+      await ctx.Channel.SendMessageAsync(embed: embed);
+    }
+  }
+}
