08cd2123938a3a954d5f967b09fe31dee925f5ee
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-12-05 18:43:51 +0000

    feat(links): class for storing links for messages

---
+++ LeafBot/LeafBot/Data/Links.cs
@@ -0,0 +1,12 @@
+﻿namespace LeafBot.Data
+{
+  public class Links
+  {
+    // sleepy bunnies
+    public static string[ ] SLEEPY_BUNS = {
+      "https://user-images.githubusercontent.com/7717434/101260094-36583480-3725-11eb-8e66-71887dc2ab3d.jpg",
+      "https://user-images.githubusercontent.com/7717434/101260102-45d77d80-3725-11eb-8c92-f04de201cbcc.jpg",
+      "https://user-images.githubusercontent.com/7717434/101260124-73bcc200-3725-11eb-8736-4b283e7787c8.jpg"
+    };
+  }
+}
