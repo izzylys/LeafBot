d095e0a3a3841d44c758ec401c02109f60100a1b
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-12-05 18:59:40 +0000

    feat(sleepy): more sleepy bun photos

--- LeafBot/LeafBot/Data/Links.cs
+++ LeafBot/LeafBot/Data/Links.cs
@@ -6,7 +6,11 @@
     public static string[ ] SLEEPY_BUNS = {
       "https://user-images.githubusercontent.com/7717434/101260094-36583480-3725-11eb-8e66-71887dc2ab3d.jpg",
       "https://user-images.githubusercontent.com/7717434/101260102-45d77d80-3725-11eb-8c92-f04de201cbcc.jpg",
-      "https://user-images.githubusercontent.com/7717434/101260124-73bcc200-3725-11eb-8736-4b283e7787c8.jpg"
+      "https://user-images.githubusercontent.com/7717434/101260124-73bcc200-3725-11eb-8736-4b283e7787c8.jpg",
+      "https://user-images.githubusercontent.com/7717434/101260923-32c7ac00-372b-11eb-8ece-c8fcdea253d9.jpg",
+      "https://user-images.githubusercontent.com/7717434/101260947-525ed480-372b-11eb-923f-2dfcd0bd26e3.jpg",
+      "https://user-images.githubusercontent.com/7717434/101261015-da44de80-372b-11eb-9431-17029e5acbd8.jpg",
+      "https://user-images.githubusercontent.com/7717434/101260995-aff32100-372b-11eb-9e12-88b740d58e53.jpg",
     };
   }
 }
