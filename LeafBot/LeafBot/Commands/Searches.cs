ef10872417bc88b18bcbaa96f9d5261f4429b873
Matthew Carney <matthewcarney64@gmail.com>
2020-12-06 12:50:51 +0000

    Added urban dictionary search command ported over from PotatoBot (RIP)

--- LeafBot/LeafBot/Commands/Searches.cs
+++ LeafBot/LeafBot/Commands/Searches.cs
@@ -3,12 +3,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
 
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.CommandsNext.Attributes;
 
 using Newtonsoft.Json;
 
+using LeafBot.Data;
 using LeafBot.Data.Models;
 
 
@@ -39,13 +41,58 @@ namespace LeafBot.Commands
         if (data.Query.Pages.First().Missing || string.IsNullOrWhiteSpace(data.Query.Pages.First().FullUrl))
         {
           DiscordEmoji emoji = DiscordEmoji.FromName(ctx.Client, ":cry:");
-          await ctx.Channel.SendMessageAsync($"Could not find anything on Wikipedia for {query} {emoji}");
+          await ctx.Channel.SendMessageAsync($"I could not find anything on Wikipedia for {query} {emoji}");
         }
         else
         {
           // Display first result
           DiscordEmoji emoji = DiscordEmoji.FromName(ctx.Client, ":books:");
           await ctx.Channel.SendMessageAsync($"{emoji} {data.Query.Pages.First().FullUrl}");
+        }
+      }
+    }
+
+    [Command("urbandictionary")]
+    [Description("Instructs leafbot to search for a term on urban dictionary (wow really high class humour you got there bud)")]
+    [Aliases("ud", "lookup", "define")]
+    public async Task UrbanDictionary(CommandContext ctx, [RemainingText] string query = null)
+    {
+      query = query?.Trim();
+      if (string.IsNullOrWhiteSpace(query))
+        return;
+
+      using (var webClient = new HttpClient())
+      {
+        // Query UrbanDictionary api
+        string response = await webClient.GetStringAsync("http://api.urbandictionary.com/v0/define?term=" + Uri.EscapeDataString(query));
+
+        // Convert response to object
+        var data = JsonConvert.DeserializeObject<UrbanDictionaryApiResponse>(response).List;
+
+        if (data.Any())
+        {
+          // Build an embed to display the results
+          var embed = new DiscordEmbedBuilder
+          {
+            Title = $"'{data.First().Word}'",
+
+            // I think it looks better without the thumbnail for now
+            //Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
+            //{
+            //  Url = Links.URBAN_DICTIONARY_ICON
+            //};
+        };
+
+          // Look I know that inlining is gross, don't judge
+          embed.AddField("Definition", data.First().Definition.Replace("[", "").Replace("]", ""));
+          embed.AddField("Example", Formatter.BlockCode(data.First().Example.Replace("[", "").Replace("]", "")));
+
+          await ctx.Channel.SendMessageAsync(embed: embed);
+        }
+        else
+        {
+          DiscordEmoji emoji = DiscordEmoji.FromName(ctx.Client, ":cry:");
+          await ctx.Channel.SendMessageAsync($"I could not find anything on Urban Dictionary for {query} {emoji}");
         }
       }
     }
---
+++ LeafBot/LeafBot/Data/Models/UrbanDictionaryApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeafBot.Data.Models
+{
+  public class UrbanDictionaryApiResponse
+  {
+    public UrbanDictionaryItem[] List { get; set; }
+  }
+  public class UrbanDictionaryItem
+  {
+    public string Word;
+    public string Definition;
+    public string PermaLink;
+    public string Example;
+  }
+}
