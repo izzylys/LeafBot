adb3bd3ca81eecb2781c1a6aaa4c092b03f4e90e
Matthew Carney <matthewcarney64@gmail.com>
2020-12-06 01:47:05 +0000

    Added role picker command (!role) Added Phrases.cs to hold any sets of phrase strings

--- LeafBot/LeafBot/Commands/Games.cs
+++ LeafBot/LeafBot/Commands/Games.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+
+using LeafBot.Data;
 
 namespace LeafBot.Commands
 {
@@ -38,6 +43,52 @@ namespace LeafBot.Commands
       }
 
       await ctx.RespondAsync($"{ctx.Member.Mention} flips a cabbage leaf {emoji} ... {outcome}! ");
+    }
+
+    [Command("role")]
+    [Description("LeafBot will decide what role you should queue as, indecisive fool...")]
+    [Aliases("icantdecide", "pickrole", "halp", "queue")]
+    public async Task PickRole(CommandContext ctx)
+    {
+      DiscordEmoji damageEmoji = DiscordEmoji.FromName(ctx.Client, ":crossed_swords:");
+      DiscordEmoji tankEmoji = DiscordEmoji.FromName(ctx.Client, ":shield:");
+      DiscordEmoji healerEmoji = DiscordEmoji.FromName(ctx.Client, ":medical_symbol:");
+
+      // setup the choices (the keys are the _key_ to this)
+      // (I hate myself)
+      Dictionary<int, string> roles = new Dictionary<int, string>
+      {
+        {1, $"Tank {tankEmoji}"},
+        {2, $"Damage {damageEmoji}"},
+        {3, $"Healer {healerEmoji}" }
+      };
+
+      // Choose random number of roles to pick, randomly order roles, pick random number of items and order based on key
+      int numberOfRoles = random.Next(1, 4);
+      var picks = roles.OrderBy(x => random.Next()).Take(numberOfRoles).OrderBy(x => x.Key).ToArray();
+
+      // Go through each picked role and HARDCODE the commas and 'and's
+      // I hate this so much, but it's late and I need to sleep
+      string result = "";
+      int count = 0;
+      foreach (var pick in picks)
+      {
+        count++;
+        result += pick.Value;
+        if (picks.Length == 2 && count == 1)
+        {
+          result += " and ";
+        }
+        else if (picks.Length > 2 && count == 2)
+        {
+          result += " and ";
+        }
+        else if (picks.Length > 2 && count == 1)
+        {
+          result += ", ";
+        }
+      }
+      await ctx.RespondAsync($"{ctx.Member.Mention}, you will queue for {result}. {Phrases.COMMANDING_PHRASES[random.Next(Phrases.COMMANDING_PHRASES.Length)]}!");
     }
   }
 }
---
+++ LeafBot/LeafBot/Data/Phrases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeafBot.Data
+{
+  public class Phrases
+  {
+    public static string[ ] COMMANDING_PHRASES =
+    {
+      "LeafBot commands it",
+      "Do it for the buns",
+      "This is the way",
+      "May the lettuce be with",
+      "The bunnies command it",
+      "You must walk this path",
+      "Do it",
+      "Go forth",
+      "This is the word of the LeafBot"
+    };
+  }
+}
